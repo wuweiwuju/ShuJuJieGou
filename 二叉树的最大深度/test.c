1ba43653ceedd034926387d88cf7b069a7c0773e
李鹏飞 <305086028@qq.com>
2019-07-23 16:44:09 +0800

    上传代码

---
+++ 二叉树的最大深度/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+//
+//int maxDepth(struct TreeNode* root) {
+//	if (root == NULL)
+//	{
+//		return 0;
+//	}
+//	int leftDepth = maxDepth(root->left);
+//	int rightDepth = maxDepth(root->right);
+//	return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+//}
