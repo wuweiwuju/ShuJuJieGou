8f3c932d2e33132de69a985d90cdc88bc4489f45
李鹏飞 <305086028@qq.com>
2019-07-23 16:44:44 +0800

    上传代码

---
+++ 相同的树/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+//µÝ¹éÔËÓÃ
+
+//bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+//	if (p == NULL && q == NULL)
+//	{
+//		return true;
+//	}
+//
+//	if (p == NULL || q == NULL)
+//	{
+//		return false;
+//	}
+//
+//	if (p->val != q->val)
+//	{
+//		return false;
+//	}
+//
+//	return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+//
+//}
+
