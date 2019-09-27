4db2d98598c494a3f1d156436deef5bdf2259a8a
李鹏飞 <305086028@qq.com>
2019-09-27 22:45:23 +0800

    上传代码

--- 插入排序/test.c
+++ 插入排序/test.c
@@ -7,7 +7,7 @@
 //#include<stdlib.h>
 #include<iostream>
 using namespace std;
-//void sort(int a[], int sz)
+////void sort(int a[], int sz)
 //{
 //	int i = 0;
 //	for (i = 0; i < sz; i++)
@@ -25,35 +25,34 @@ using namespace std;
 //}
 //直接插入排序  稳定的排序  时间复杂度O(n^2)  空间复杂度O(1)
 //适用场景：数据量比较小，接近有序
-//void Insertsort(int* array,int sz)
-//{
-//	for (int i = 1; i < sz; i++)
-//	{
-//		int key = array[i];
-//		int end = i - 1;
-//		//找待插入的位置  O(n)
-//		while (end >= 0 && key < array[end])
-//		{
-//			array[end + 1] = array[end];
-//			--end;
-//		}
-//		//插入数据
-//		array[end + 1] = key;
-//	}
-//}
-//int main()
-//{
-//	int arr[] = { 1,5,2,6,9,7,4,3,10 };
-//	int size = sizeof(arr) / sizeof(int);
-//	Insertsort(arr, size);
-//	for (int i = 0; i < size; i++)
-//	{
-//		printf("%d ", arr[i]);
-//	}
-//	printf("\n");
-//	system("pause");
-//	return 0;
-//}
+void Insertsort(int* array,int sz)
+{
+	for (int i = 1; i < sz; i++)
+	{
+		int key = array[i];
+		int end = i - 1;
+		//找待插入的位置  O(n)
+		while (end >= 0 && key < array[end])
+		{
+			array[end + 1] = array[end];
+			--end;
+		}
+		//插入数据
+		array[end + 1] = key;
+	}
+}
+int main()
+{
+	int arr[] = { 1,5,2,6,9,7,4,3,10 };
+	int size = sizeof(arr) / sizeof(int);
+	Insertsort(arr, size);
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d", arr[i]);
+	}
+	system("pause");
+	return 0;
+}
 //目前数据场景： 数据量大，非常杂乱----按照插入数据思想进行排序
 
 //希尔排序 不稳定
@@ -62,41 +61,41 @@ using namespace std;
 //空间复杂度：O(1)
 //由于多次插入排序，我们知道一次插入排序是稳定的，不会改变相同元素的相对顺序，但在不同的插入排序过程中，相同的元素可能在各自的插入排序中移动，最后其稳定性就会被打乱，所以shell排序是不稳定的。
 //场景：数据量大  杂乱的排序
-void Shellsor(int* array, int sz)
-{
-	int gap = sz; // 10  4  2  1
-	//while (gap != gap / 3 + 1)
-	while(gap > 1)
-	{  
-		gap = gap / 3 + 1;
-		for (int i = gap; i < sz; i++)
-		{
-			int key = array[i];
-			int end = i - gap;
-			//找待插入的位置  O(n)
-			while (end >= 0 && key < array[end])
-			{
-				array[end + gap] = array[end];
-				end -= gap;
-			}
-			//插入数据
-			array[end + gap] = key;
-		}
-	}
-}
-int main()
-{
-	int arr[] = { 1,5,2,6,9,7,4,3,10 };
-	int size = sizeof(arr) / sizeof(int);
-	Shellsor(arr, size);
-	/*for (int i = 0; i < size; i++)
-	{
-		printf("%d ", arr[i]);
-	}*/
-	for (auto i : arr)
-	{
-		cout << i << " ";
-	}
-	system("pause");
-	return 0;
-}
+//void Shellsor(int* array, int sz)
+//{
+//	int gap = sz; // 10  4  2  1
+//	//while (gap != gap / 3 + 1)
+//	while(gap > 1)
+//	{  
+//		gap = gap / 3 + 1;
+//		for (int i = gap; i < sz; i++)
+//		{
+//			int key = array[i];
+//			int end = i - gap;
+//			//找待插入的位置  O(n)
+//			while (end >= 0 && key < array[end])
+//			{
+//				array[end + gap] = array[end];
+//				end -= gap;
+//			}
+//			//插入数据
+//			array[end + gap] = key;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1,5,2,6,9,7,4,3,10 };
+//	int size = sizeof(arr) / sizeof(int);
+//	Shellsor(arr, size);
+//	/*for (int i = 0; i < size; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}*/
+//	for (auto i : arr)
+//	{
+//		cout << i << " ";
+//	}
+//	system("pause");
+//	return 0;
+//}
